a37f60c58e63a7b541c1277f0eb8daece9915dee
reddevx <juan.devx@gmail.com>
2024-07-21 11:43:45 -0300

    [add] hash for dicionary

--- models/menu.rb
+++ models/menu.rb
@@ -3,30 +3,26 @@ class Menu
     @controller = PersonsController.new
     
     @options = {
-        1 => -> { @controller.fetch_person }, 
-        2 => -> { @controller.input_imc }, 
-        3 => -> { @controller.input_pam }, 
-        4 => -> { @controller.display_person }, 
-        5 => -> { end_app }
-      }
-
-    @options.default = -> { puts "Opção inválida.\n" }
+      1 => { label: 'Cadastrar pessoa', action: -> { @controller.fetch_person  } },
+      2 => { label: 'Calcular IMC', action: -> { @controller.input_imc  } },
+      3 => { label: 'Calcular Pam', action: -> { @controller.input_pam  } },
+      4 => { label: 'Exibir informações', action: -> { @controller.display_person  } },
+      5 => { label: 'Sair', action: -> { end_app }  }
+    }
+    
+    @options.default =  { action: -> { puts "Opção inválida.\n" } } 
   end
 
   def show_options
     puts 'Digite uma das opções abaixo:'
-    puts '1. Cadastrar Pessoa'
-    puts '2. Calcular IMC'
-    puts '3. Calcular PAM'
-    puts '4. Exibir Informações'
-    puts '5. Sair'
+    
+    @options.each { |option| puts "#{option.first}.#{option.last[:label]}" }
   end
-
 
   def select_option
     @option = gets.chomp.to_i
 
-    @options[@option].call
+    @options[@option][:action].call
   end
 
   def end_app
